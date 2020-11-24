67c8a0fe0f3d1123efa51a14b3cad038ac1b9c2d
Alexander Kramer <kramer.axs@gmail.com>
2020-11-24 00:30:41 -0600

    level 4 Salting and hashing password with bcrypt

--- controller/userController.js
+++ controller/userController.js
@@ -1,33 +1,41 @@
 const User = require('../models/User');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+
+const saltRound = 10;
 
 const registerUser = (req, res) => {
 
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-
-  newUser.save(error => {
-    if(error){
-      console.log(error)
-    }
-    else{
-      res.render('secrets');
-    }
-  });
+  bcrypt.hash(req.body.password, saltRound, (error, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+  
+    newUser.save(error => {
+      if(error){
+        console.log(error)
+      }
+      else{
+        res.render('secrets');
+      }
+    });
+  })
 }
 
 const loginUser = (req, res ) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, (error, user) => {
     if(error){
       console.log(error);
     }else{
-      if(user && user.password === password){
-        res.render('secrets')
+      if(user){
+        bcrypt.compare(password, user.password, (error, result) => {
+          if(result){
+            res.render('secrets')
+          }
+        })
       }
     }
   })
