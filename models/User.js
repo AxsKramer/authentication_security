65ccc06eb24cf616a79c4a085256baa8f60ce3cf
Alexander Kramer <kramer.axs@gmail.com>
2020-11-23 20:28:53 -0600

    Level 2 encryption with mongoose-encryption

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -14,5 +15,11 @@ const userSchema = new Schema({
     trim: true
   }
 });
+
+//Mongoose encrypt the password when post user password to db
+//and unencrypt when we use find() function in the model
+
+const secret = 'Thisisoursecret';
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password'] });
 
 module.exports = new mongoose.model('User', userSchema);
